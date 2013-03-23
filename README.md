34e920f4324b6748683fe1027eeade31863f9b8d
Bashanta Dahal <bashanta@1streamline.com>
2013-03-24 01:24:26 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Chat Application Using Dart Programming Language
---
+++ file-logger.dart
@@ -0,0 +1,32 @@
+library filelogger;
+import 'dart:isolate';
+import 'dart:io';
+import 'server-utils.dart';
+
+startLogging() {
+  print('started logger');
+  File logFile;
+  IOSink out;
+  port.receive((msg, replyTo) {
+    if (logFile == null) {
+      print("Opening file $msg");
+      logFile = new File(msg);
+      out = logFile.openWrite(FileMode.APPEND);
+    } else {
+      time('write to file', () {
+        out.addString("${new DateTime.now()} : $msg\n");
+      });
+    }
+  });
+}
+
+SendPort _loggingPort;
+
+void log(String message) {
+  _loggingPort.send(message);
+}
+
+void initLogging(String logFileName) {
+  _loggingPort = spawnFunction(startLogging);
+  _loggingPort.send(logFileName);
+}
